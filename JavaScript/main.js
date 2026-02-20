0e9d91d0f9d832b93a921c9399680cc17cbbceab
Rubayed Ahmed Foysal <rubayedahmedfoysal@gmial.com>
2026-02-21 02:52:18 +0600

    update

---
+++ JavaScript/main.js
@@ -0,0 +1,62 @@
+// console.log("Hello world")
+
+// let x = 10;
+// console.log(x);
+
+// number
+// let a = 20;
+// let b = 33.99;
+
+// console.log(a, b)
+
+// string
+let s = "Hello World!";
+s[0] = 'a';
+// let name = 'Foysal';
+console.log(s);
+
+// boolean
+
+// let flag = true;
+// let login = false;
+
+// console.log(flag, login);
+
+// BigInt
+
+// let n = 13893829938119193844n;
+// console.log(n);
+
+// undefined
+// let h;
+// console.log(h);
+
+// null
+// let copy = null;
+// console.log(copy);
+
+// Array
+// let arr = [1, 2, 3, "Foysal", true];
+// console.log(arr);
+
+// object
+let obj = {
+    name: "Foysal",
+    account: 1200,
+    age: 23,
+};
+
+let obj2 = obj;
+obj2.name = "Ahmed";
+
+console.log(obj);
+
+// function
+// let s = function add() {
+//     console.log(20 + 3);
+
+// }
+
+// s();
+
+
