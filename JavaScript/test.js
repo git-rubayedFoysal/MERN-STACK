f47c7315591e29166dd2f72af7a6a6eef7bfe6d2
Rubayed Ahmed Foysal <rubayedahmedfoysal@gmial.com>
2026-02-26 21:24:34 +0600

    update

---
+++ JavaScript/test.js
@@ -0,0 +1,70 @@
+//
+// console.log("hello" >= undefined);
+
+//  a = 2 -> 0010
+// b = 5 -> 1001
+
+// a&b = 2&5 = 0000
+
+// console.log(2 & 3)
+
+// console.log(Math.floor(Math.random() * 6) + 1);
+
+// console.log(Math.floor(Math.random() * 11) + 15);
+
+const str = ` Hello World! World! `;
+
+// console.log(str.toLowerCase());
+// console.log(str.includes('llo'));
+
+// console.log(str.slice(-9));
+// console.log(str.substring(3, 9));
+
+// const str1 = `I am programmer.`
+// const str2 = str + " " + str1;
+
+// console.log(str.replace("Wor", "Mor"));
+// console.log(str.replaceAll("Wor", "Mor"));
+
+
+
+// console.log(str.trim());
+
+// const user = `Rubayed Ahmed Foysal Programmer Coder`;
+// console.log(user.split(" "))
+
+// const about = `I am ${user}`;
+// console.log(about);
+
+// const now = new Date();
+
+// console.log(now.toLocaleString());
+
+// const now = Date.now();
+
+const date = new Date();
+
+// const a = new Date(2025, 1, 30); // Feb 30? Becomes March 2!
+// const b = new Date('2025-02-30'); // Invalid Date
+
+// Inconsistent behavior
+new Date(2025, 0); // Jan 1, 2025 00:00:00
+const b = new Date(2025);    // Jan 1, 1970 plus 2025 milliseconds! 😱
+
+// console.log(b.toString());
+
+let dec = 9;
+let hex = 0xf;
+let bin = 0b110;
+let oct = 0o7;
+
+// console.log(dec, hex, bin, oct);
+
+console.log();
+
+
+
+
+
+
+
